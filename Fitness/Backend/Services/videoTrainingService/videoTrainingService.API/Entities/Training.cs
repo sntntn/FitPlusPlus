a04d8241576c48e7145b8a2c8f8a658465e216c2
NataMata <natafil20@gmail.com>
2024-12-12 23:29:26 +0100

    Fix: field order changed

--- Fitness/Backend/Services/videoTrainingService/videoTrainingService.API/Entities/Training.cs
+++ Fitness/Backend/Services/videoTrainingService/videoTrainingService.API/Entities/Training.cs
@@ -2,7 +2,7 @@ namespace videoTrainingService.API.Entities;
 
 public class Training
 {
+    public string TrainingId { get; set; }
     public string ClientId { get; set; }
     public string TrainerId { get; set; }
-    public string TrainingId { get; set; }
 }
