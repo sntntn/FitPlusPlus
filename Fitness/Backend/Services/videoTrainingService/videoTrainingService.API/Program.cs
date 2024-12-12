76fc04790f9fb4faa448422bbf4de13531c5a421
NataMata <natafil20@gmail.com>
2024-12-12 23:30:02 +0100

    Feat: added context class and required settings

--- Fitness/Backend/Services/videoTrainingService/videoTrainingService.API/Program.cs
+++ Fitness/Backend/Services/videoTrainingService/videoTrainingService.API/Program.cs
@@ -1,6 +1,11 @@
+using videoTrainingService.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddScoped<ITrainingContext, TrainingContext>();
+ 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
