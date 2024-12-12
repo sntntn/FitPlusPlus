601ba1bc0d0e967c8a7e942130ad5464b7c477eb
NataMata <natafil20@gmail.com>
2024-12-12 21:08:03 +0100

    Feat: added data models

---
+++ Fitness/Backend/Services/videoTrainingService/videoTrainingService.API/Entities/Training.cs
@@ -0,0 +1,8 @@
+namespace videoTrainingService.API.Entities;
+
+public class Training
+{
+    public string ClientId { get; set; }
+    public string TrainerId { get; set; }
+    public string TrainingId { get; set; }
+}
