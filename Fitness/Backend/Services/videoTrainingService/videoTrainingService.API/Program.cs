66d4d4fb29c1477e09eb5d75ac06294e07bfc34d
NataMata <natafil20@gmail.com>
2024-12-14 14:21:42 +0100

    Fix: deleted generic code and modified to enable the project to work

--- Fitness/Backend/Services/videoTrainingService/videoTrainingService.API/Program.cs
+++ Fitness/Backend/Services/videoTrainingService/videoTrainingService.API/Program.cs
@@ -9,6 +9,8 @@ builder.Services.AddScoped<ITrainingContext, TrainingContext>();
 builder.Services.AddScoped<ITrainingRepository, TrainingRepository>();
  
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -21,31 +23,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
+app.MapControllers();
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
