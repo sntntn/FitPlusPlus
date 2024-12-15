f08215fd0ece9dbbb747228e20b098332d90a20c
NataMata <natafil20@gmail.com>
2024-12-15 22:12:52 +0100

    Feat: implemented all required methods in controller

--- Fitness/Backend/Services/videoTrainingService/videoTrainingService.API/Repositories/ITrainingRepository.cs
+++ Fitness/Backend/Services/videoTrainingService/videoTrainingService.API/Repositories/ITrainingRepository.cs
@@ -5,7 +5,7 @@ namespace videoTrainingService.API.Repositories
     public interface ITrainingRepository
     {
         Task<IEnumerable<Exercise>> GetExercises(string trainerId);
-        Task<Exercise> GetExercise(string name, string trainerId);
+        Task<Exercise> GetExercise(string id);
         Task CreateExercise(Exercise exercise);
         Task<bool> UpdateExercise(Exercise exercise);
         Task<bool> DeleteExercise(string id);
@@ -16,8 +16,10 @@ namespace videoTrainingService.API.Repositories
         Task<bool> UpdateTraining(Training training);
         Task<bool> DeleteTraining(string id);
         Task<IEnumerable<TrainingExercise>> GetTrainingExercises(string trainingId);
+        Task<TrainingExercise> GetTrainingExercise(string id);
         Task CreateTrainingExercise(TrainingExercise trainingExercise);
         Task<bool> UpdateTrainingExercise(TrainingExercise trainingExercise);
-        Task<bool> DeleteTrainingExercise(string trainingId, string id);
+        Task<bool> DeleteTrainingExercise(string id);
+        
     }
 }
