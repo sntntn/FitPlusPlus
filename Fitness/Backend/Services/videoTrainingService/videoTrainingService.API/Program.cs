76ac49d95f1d7be7b445929f37c978fdc41a4c0b
NataMata <natafil20@gmail.com>
2025-01-16 12:41:34 +0100

    Fix: changed attributes due to routing error.

--- Fitness/Backend/Services/videoTrainingService/videoTrainingService.API/Program.cs
+++ Fitness/Backend/Services/videoTrainingService/videoTrainingService.API/Program.cs
@@ -23,11 +23,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-//app.UseHttpsRedirection();
-
-//app.UseAuthorization();
-
+app.UseHttpsRedirection();
 app.MapControllers();
-
 app.Run();
 
