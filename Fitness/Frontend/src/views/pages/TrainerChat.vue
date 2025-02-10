aa59ee7c2ae1a28f1976fee0c1796dd56c379ace
Vukasin <skolskiacc@gmail.com>
2025-02-10 13:22:11 +0100

    Filter incoming WebSocket messages by sender type on TrainerPage
    - Display only messages from the client side to prevent double messages
      from trainer side
    
    Relates to #67

--- Fitness/Frontend/src/views/pages/TrainerChat.vue
+++ Fitness/Frontend/src/views/pages/TrainerChat.vue
@@ -202,12 +202,13 @@ export default {
         const messageData = JSON.parse(event.data);
         console.log("New message received:", messageData);
 
-
-        if (this.selectedClient && this.selectedClient.id === clientId) {
+        // da bi mi se ispisivale poruke koje stizu u web socket ali samo sa suprotne strane (sa strane klijenta) 
+        if (this.selectedClient && this.selectedClient.id === clientId && messageData.senderType=="client") {
           this.selectedClient.messages.push({
-            id: messageData.id || Date.now(),
-            text: messageData.text,
-            sender: messageData.sender.toLowerCase(),
+            id: messageData.Id || Date.now(),
+            text: messageData.Content,
+            sender: messageData.senderType.toLowerCase(),
+            //sender: typeof messageData.senderType === "string" ? messageData.senderType.toLowerCase() : "unknown",
           });
         }
       };
