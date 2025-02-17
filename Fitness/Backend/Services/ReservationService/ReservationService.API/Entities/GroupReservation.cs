b2249a90d4ddd24192c3ad1c5ce669792bc23f53
milanmitreski <milanmitreski@gmail.com>
2025-02-17 14:26:40 +0100

    Basic skeleton for Reservation Service. Relates to #74.

---
+++ Fitness/Backend/Services/ReservationService/ReservationService.API/Entities/GroupReservation.cs
@@ -0,0 +1,20 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ReservationService.API.Entities;
+
+public class GroupReservation
+{
+    [BsonId] 
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+
+    public string Name { get; set; }
+    public string About { get; set; }
+    public string TrainerId { get; set; }
+    public int Capacity { get; set; }
+    public List<string> Clients { get; set; }
+    public TimeSpan StartTime { get; set; }
+    public TimeSpan EndTime { get; set; }
+    public DateOnly Date { get; set; }
+}
