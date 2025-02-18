12483b21da19c63baaa36bea1271e945f5affe7e
stemil01 <s.milenkovic@protonmail.com>
2025-02-18 21:11:09 +0100

    Add CORS in analytics service and first non-hardcoded data. Relates to #69.

--- Fitness/Frontend/src/services/AnalyticsService.js
+++ Fitness/Frontend/src/services/AnalyticsService.js
@@ -1,25 +1,33 @@
 import axios from "axios";
 
-export async function get_client_analytics(cli_id) {
-    let harcodedObject = Promise.resolve({
-        data: {
-        attendedTrainings: 10,
-        cancelledTrainings: 2,
-        averageRating: 7.4,
-        trainersWorkedWith: [
-            {
-            fullName: "Vukasin Markovic",
-            contactEmail: "vmark@fitness.com",
-            contactPhone: "+38160123456",
-            trainingTypes: [
-                { name: "yoga" },
-                { name: "pilates "}
-            ],
-            averageRating: 10.0
-            }
-        ]
-        }
-    });
+const ANALYTICS_URL = "http://localhost:8005/api/v1/Analytics";
 
-    return harcodedObject;
+const analyticsService = {
+    async getClientAnalytics(clientId) {
+        let harcodedObject = Promise.resolve({
+            data: {
+            attendedTrainings: 10,
+            cancelledTrainings: 2,
+            averageRating: 7.4,
+            trainersWorkedWith: [
+                {
+                fullName: "Vukasin Markovic",
+                contactEmail: "vmark@fitness.com",
+                contactPhone: "+38160123456",
+                trainingTypes: [
+                    { name: "yoga" },
+                    { name: "pilates "}
+                ],
+                averageRating: 10.0
+                }
+            ]
+            }
+        });
+
+        axios.defaults.headers.common = { 'Authorization': `Bearer ${sessionStorage.getItem('accessToken')}` };
+        let response = await axios.get(`${ANALYTICS_URL}/GetClientNumOfTrainingForClient?clientId=${clientId}`);
+        return response;
+    }
 }
+
+export default analyticsService;
