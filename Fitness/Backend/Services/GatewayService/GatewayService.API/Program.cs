99e8cf484d0a2e494f41cdf21d251879637fc40a
Vukasin Markovic <skolskiacc@gmail.com>
2025-02-18 12:45:57 +0100

    Implemented CORS in Gateway and updated frontend API endpoints
    Relates to #77.

--- Fitness/Backend/Services/GatewayService/GatewayService.API/Program.cs
+++ Fitness/Backend/Services/GatewayService/GatewayService.API/Program.cs
@@ -9,6 +9,15 @@ builder.Configuration
     .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
     .AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", optional: false, reloadOnChange: true);
 
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy",
+        builder => builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -23,6 +32,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("CorsPolicy");
 
 app.UseRouting();
 app.UseOcelot().Wait();
