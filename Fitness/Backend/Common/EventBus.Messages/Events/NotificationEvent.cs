0a41dec8a9bc449624a79612ada4c84714c8ad18
milanmitreski <milanmitreski@gmail.com>
2025-02-18 19:15:07 +0100

    NotificationEvent implemented. NotificationService added to docker-compose.yml and docker-compose.override.yml. Relates to #76.

--- Fitness/Backend/Common/EventBus.Messages/Events/NotificationEvent.cs
+++ Fitness/Backend/Common/EventBus.Messages/Events/NotificationEvent.cs
@@ -2,19 +2,12 @@ namespace EventBus.Messages.Events;
 
 public class NotificationEvent : IntegrationBaseEvent
 {
-    public string UserId { get; set; }
-    public UserType UType { get; set; }
+    public IDictionary<string, string> UserIdToUserType;
     public string Title { get; set; }
     public string Content { get; set; }
-    public NotificationType NType { get; set; }
+    public NotificationType Type { get; set; }
     public bool Email { get; set; }
 
-    public enum UserType
-    {
-        Client,
-        Trainer
-    }
-    
     public enum NotificationType
     {
         Information,
