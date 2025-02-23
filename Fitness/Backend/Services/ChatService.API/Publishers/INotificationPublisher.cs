96b2ce09904af29c05d2683f3f0a5aaef251eac7
milanmitreski <milanmitreski@gmail.com>
2025-02-23 14:08:35 +0100

    Notification infrastructure added to ChatService.API. Relates to #85.

---
+++ Fitness/Backend/Services/ChatService.API/Publishers/INotificationPublisher.cs
@@ -0,0 +1,6 @@
+namespace ChatService.API.Publishers;
+
+public interface INotificationPublisher
+{
+    Task PublishNotification(string title, string content, string type, bool email, IDictionary<string, string> users);
+}
---
+++ Fitness/Backend/Services/ChatService.API/Publishers/NotificationPublisher.cs
@@ -0,0 +1,34 @@
+using AutoMapper;
+using ChatService.API.Models;
+using EventBus.Messages.Events;
+using MassTransit;
+
+namespace ChatService.API.Publishers;
+
+public class NotificationPublisher : INotificationPublisher
+{
+    private readonly IPublishEndpoint _publishEndpoint;
+    private readonly IMapper _mapper;
+
+    public NotificationPublisher(IPublishEndpoint publishEndpoint, IMapper mapper)
+    {
+        _publishEndpoint = publishEndpoint ?? throw new ArgumentNullException(nameof(publishEndpoint));
+        _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+    }
+
+    public async Task PublishNotification(string title, string content, string type, bool email, IDictionary<string, string> users)
+    {
+        Enum.TryParse(type, out Notification.NotificationType notificationType);
+        var notification = new Notification
+        {
+            Title = title,
+            Content = content,
+            Type = notificationType,
+            Email = email,
+            UserIdToUserType = users
+        };
+
+        var eventMessage = _mapper.Map<NotificationEvent>(notification);
+        await _publishEndpoint.Publish(eventMessage);
+    }
+}
