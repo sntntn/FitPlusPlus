d8d8f55cd17d6abfe1826665e053c48929591555
anjaanjaa10 <anjacolic2000@gmail.com>
2025-09-28 02:03:06 +0200

    Init NutritionService

---
+++ Fitness/Backend/Services/NutritionService/NutritionService.API/Controllers/HealthController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace NutritionService.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok("NutritionService running..");
+    }
+}
---
+++ Fitness/Backend/Services/NutritionService/NutritionService.API/Program.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(o =>
+{
+    o.AddPolicy("CorsPolicy", p => p.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+
+var app = builder.Build();
+
+app.UseCors("CorsPolicy");
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+
+app.Run();
