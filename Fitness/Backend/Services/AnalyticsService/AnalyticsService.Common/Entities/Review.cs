b5126157970880747b99fe7060e694d203480c57
stemil01 <s.milenkovic@protonmail.com>
2025-10-12 12:10:37 +0200

    Configure anlytics service consumer part for reviews. Relates to #114.

--- Fitness/Backend/Services/AnalyticsService/AnalyticsService.Common/Entities/Review.cs
+++ Fitness/Backend/Services/AnalyticsService/AnalyticsService.Common/Entities/Review.cs
@@ -5,11 +5,7 @@ namespace AnalyticsService.Common.Entities;
 
 public class Review
 {
-    [BsonId]
-    [BsonRepresentation(BsonType.ObjectId)]
-    public string Id { get; set; }
-    public string ReservationId { get; set; }
-    public string UserId { get; set; }
-    public int Rating { get; set; }
-    public string Comment { get; set; }
+    public string? UserId { get; set; }
+    public int? Rating { get; set; }
+    public string? Comment { get; set; }
 }
