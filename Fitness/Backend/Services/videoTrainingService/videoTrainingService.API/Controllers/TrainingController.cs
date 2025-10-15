8fda4465a8929717f54b46a9037a88db357a0ca6
NataMata <natafil20@gmail.com>
2025-10-15 23:39:07 +0200

    Added Doxygen comments.

--- Fitness/Backend/Services/videoTrainingService/videoTrainingService.API/Controllers/TrainingController.cs
+++ Fitness/Backend/Services/videoTrainingService/videoTrainingService.API/Controllers/TrainingController.cs
@@ -17,7 +17,11 @@ namespace videoTrainingService.API.Controllers
         {
             _repository = repository ?? throw new ArgumentNullException(nameof(repository));
         }
-        
+
+        /// <summary>
+        /// API for retrieval of added exercises from specific trainer.
+        /// </summary> 
+        /// <param name="trainerId">Unique identifier of the desired trainer.</param>
         [Authorize(Roles = "Admin, Trainer, Client")]
         [HttpGet("exercises/{trainerId}")]
         [ProducesResponseType(typeof(IEnumerable<Exercise>), StatusCodes.Status200OK)]
@@ -27,6 +31,10 @@ namespace videoTrainingService.API.Controllers
             return Ok(exercises);
         }
         
+        /// <summary>
+        /// API for retrieval of one specific exercise.
+        /// </summary> 
+        /// <param name="id">Unique identifier of the desired exercise.</param>
         [Authorize(Roles = "Admin, Trainer, Client")]
         [HttpGet("exercise/{id}", Name = "GetExercise")]
         [ProducesResponseType(typeof(Exercise), StatusCodes.Status200OK)]
@@ -40,7 +48,11 @@ namespace videoTrainingService.API.Controllers
             }
             return Ok(exercise);
         }
-        
+
+        /// <summary>
+        /// API for addition of new exercises by trainer.
+        /// </summary> 
+        /// <param name="exercise">Object that represents exercise and contains values for exercise name and video path.</param>
         [Authorize(Roles = "Admin, Trainer")]
         [HttpPost("exercise")]
         [ProducesResponseType(typeof(Exercise), StatusCodes.Status201Created)]
@@ -50,6 +62,10 @@ namespace videoTrainingService.API.Controllers
             return CreatedAtRoute("GetExercise", new { id = exercise.Id} , exercise);
         }
 
+        /// <summary>
+        /// API for exercise update.
+        /// </summary> 
+        /// <param name="exercise">Object that represents exercise with updated values.</param>
         [Authorize(Roles = "Admin, Trainer")]
         [HttpPut("exercise")]
         [ProducesResponseType(typeof(Exercise), StatusCodes.Status200OK)]
@@ -59,6 +75,10 @@ namespace videoTrainingService.API.Controllers
             return Ok(result);
         }
         
+        /// <summary>
+        /// API for exercise removal.
+        /// </summary> 
+        /// <param name="id">Unique identifier of the desired exercise.</param>
         [Authorize(Roles = "Admin, Trainer")]
         [HttpDelete("exercise/{id}")]
         [ProducesResponseType(typeof(Exercise), StatusCodes.Status200OK)]
@@ -68,6 +88,9 @@ namespace videoTrainingService.API.Controllers
             return Ok(result);
         }
         
+        /// <summary>
+        /// API for retrieval of trainings for clients. Retrives all trainings in database.
+        /// </summary> 
         [Authorize(Roles = "Admin, Trainer, Client")]
         [HttpGet("training/trainingClient", Name = "GetTrainingsForClient")]
         [ProducesResponseType(typeof(IEnumerable<Training>), StatusCodes.Status200OK)]
@@ -77,6 +100,10 @@ namespace videoTrainingService.API.Controllers
             return Ok(trainings);
         }
         
+        /// <summary>
+        /// API for retrieval of trainings for trainer.
+        /// </summary>
+        /// <param name="trainerId">Unique identifier of the desired trainer.</param>
         [Authorize(Roles = "Admin, Trainer, Client")]
         [HttpGet("training/trainingTrainer/{trainerId}", Name = "GetTrainingsForTrainer")]
         [ProducesResponseType(typeof(IEnumerable<Training>), StatusCodes.Status200OK)]
@@ -86,6 +113,10 @@ namespace videoTrainingService.API.Controllers
             return Ok(trainings);
         }
         
+        /// <summary>
+        /// API for retrieval of specific training.
+        /// </summary>
+        /// <param name="id">Unique identifier of the desired training.</param>
         [Authorize(Roles = "Admin, Trainer, Client")]
         [HttpGet("training/{id}", Name = "GetTraining")]
         [ProducesResponseType(typeof(Training), StatusCodes.Status200OK)]
@@ -100,6 +131,11 @@ namespace videoTrainingService.API.Controllers
             return Ok(training);
         }
         
+        /// <summary>
+        /// API for addition of new training made by trainer.
+        /// </summary> 
+        /// <param name="training">Object that represents training with values for trainer id, 
+        /// training type and description.</param>
         [Authorize(Roles = "Admin, Trainer")]
         [HttpPost("training")]
         [ProducesResponseType(typeof(Training), StatusCodes.Status201Created)]
@@ -109,6 +145,10 @@ namespace videoTrainingService.API.Controllers
             return CreatedAtRoute("GetTraining", new { id = training.TrainingId} , training);
         }
 
+        /// <summary>
+        /// API for training update.
+        /// </summary> 
+        /// <param name="training">Object with updated values.</param>
         [Authorize(Roles = "Admin, Trainer")]
         [HttpPut("training")]
         [ProducesResponseType(typeof(Training), StatusCodes.Status200OK)]
@@ -118,6 +158,10 @@ namespace videoTrainingService.API.Controllers
             return Ok(result);
         }
         
+        /// <summary>
+        /// API for training removal.
+        /// </summary> 
+        /// <param name="id">Unique identifier of the desired training.</param>
         [Authorize(Roles = "Admin, Trainer")]
         [HttpDelete("training/{id}")]
         [ProducesResponseType(typeof(Training), StatusCodes.Status200OK)]
@@ -127,6 +171,10 @@ namespace videoTrainingService.API.Controllers
             return Ok(result);
         }
 
+        /// <summary>
+        /// API for retrieval of all exercises from specific training.
+        /// </summary> 
+        /// <param name="trainingId">Unique identifier of the desired training.</param>
         [Authorize(Roles = "Admin, Trainer, Client")]
         [HttpGet("trainingExercises/{trainingId}", Name = "GetTrainingExercises")]
         [ProducesResponseType(typeof(IEnumerable<TrainingExercise>), StatusCodes.Status200OK)]
@@ -136,6 +184,10 @@ namespace videoTrainingService.API.Controllers
             return Ok(trainingExercises);
         }
         
+        /// <summary>
+        /// API for retrieval of one exercise from specific training.
+        /// </summary> 
+        /// <param name="id">Unique identifier of the desired exercise.</param>
         [Authorize(Roles = "Admin, Trainer, Client")]
         [HttpGet("trainingExercise/{id}", Name = "GetTrainingExercise")]
         [ProducesResponseType(typeof(TrainingExercise), StatusCodes.Status200OK)]
@@ -150,6 +202,11 @@ namespace videoTrainingService.API.Controllers
             return Ok(trainingExercise);
         }
         
+        /// <summary>
+        /// API for addition of new exercise in specific training.
+        /// </summary> 
+        /// <param name="trainingExercises">Object that represents exercise and contains values for training and existing exercise id and sets and reps 
+        /// number of repetitions.</param>
         [Authorize(Roles = "Admin, Trainer")]
         [HttpPost("trainingExercise")]
         [ProducesResponseType(typeof(TrainingExercise), StatusCodes.Status201Created)]
@@ -159,6 +216,10 @@ namespace videoTrainingService.API.Controllers
             return CreatedAtRoute("GetTrainingExercise", new { id = trainingExercise.Id} , trainingExercise);
         }
         
+        /// <summary>
+        /// API for training update.
+        /// </summary> 
+        /// <param name="trainingExercises">Object that represents exercise with updated values.</param>
         [Authorize(Roles = "Admin, Trainer")]
         [HttpPut("trainingExercise")]
         [ProducesResponseType(typeof(TrainingExercise), StatusCodes.Status200OK)]
@@ -168,6 +229,10 @@ namespace videoTrainingService.API.Controllers
             return Ok(result);
         }
         
+        /// <summary>
+        /// API for removal of exercises from training. Removes all exercises for desired training.
+        /// </summary> 
+        /// <param name="trainingId">Unique identifier of the desired training.</param>
         [Authorize(Roles = "Admin, Trainer")]
         [HttpDelete("trainingExercise/{trainingId}")]
         [ProducesResponseType(typeof(TrainingExercise), StatusCodes.Status200OK)]
@@ -177,6 +242,11 @@ namespace videoTrainingService.API.Controllers
             return Ok(result);
         }
         
+        /// <summary>
+        /// API for training's queue update. Adds client's unique identifier in queue of clients who purchased this training.
+        /// </summary> 
+        /// <param name="clientId">Unique identifier of client who purchased training.</param>
+        /// <param name="trainingId">Unique identifier of purchased training.</param>
         [Authorize(Roles = "Client")]
         [HttpPost("training/{trainingId}/addClient/{clientId}")]
         public async Task<IActionResult> AddClientToTraining(string trainingId, string clientId)
@@ -189,6 +259,10 @@ namespace videoTrainingService.API.Controllers
             return Ok($"Client {clientId} added to training {trainingId}.");
         }
 
+        /// <summary>
+        /// API for retrieval of purchased trainings for client.
+        /// </summary> 
+        /// <param name="clientId">Unique identifier of desired client.</param>
         [Authorize(Roles = "Admin, Trainer, Client")]
         [HttpGet("training/byClient/{clientId}")]
         public async Task<ActionResult<List<string>>> GetTrainingsByClient(string clientId)
